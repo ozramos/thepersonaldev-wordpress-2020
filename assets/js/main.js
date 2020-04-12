cad6ec5b8c8bd9389bf5a6b98c0b1701884becad
Oz Ramos <thepersonaldev@gmail.com>
2020-04-11 22:38:30 -0700

    #12 Superfish main menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
+import 'superfish'
+
 (($, wp) => {
+  $('.desktop-main-menu-wrap ul.menu').superfish()
+  
   $('.mobile-main-menu-hamburger').click(function () {
     $('body').toggleClass('mobile-main-menu-is-open')
   })
--- functions.php
+++ functions.php
@@ -9,9 +9,9 @@ add_action('wp_enqueue_scripts', function () {
 	$theme_version = wp_get_theme()->get( 'Version' );
 
   wp_enqueue_style( 'ad-hoc', get_stylesheet_uri(), [], $theme_version);
-  wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/dist/main.css', [], $theme_version);
+  wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/dist/styles/main.css', [], $theme_version);
 
-  wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/assets/js/main.js', ['jquery',], $theme_version, true );
+  wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/dist/js/main.js', ['jquery',], $theme_version, true );
   wp_enqueue_script( 'theme-customizer', get_stylesheet_directory_uri() . '/assets/js/theme-customizer.js', ['jquery', 'customize-preview'], $theme_version, true );
 });
 
