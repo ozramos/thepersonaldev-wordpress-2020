6c71ca4dd530556bdef4f66924fe831a3ed49e76
Oz Ramos <thepersonaldev@gmail.com>
2020-04-11 23:02:35 -0700

    #12 Expand/collapse mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 import 'superfish'
 
 (($, wp) => {
+  // Superfish
   $('.desktop-main-menu-wrap ul.menu').superfish()
   
+  // Hamburger
   $('.mobile-main-menu-hamburger').click(function () {
     $('body').toggleClass('mobile-main-menu-is-open')
+  })
+
+  // Submenu expand
+  $('.mobile-main-menu-wrap .menu-item-has-children > a').click(function () {
+    $(this).parent().toggleClass('sub-menu-visible')
+    return false
   })
 })(window.jQuery, window.wp)
