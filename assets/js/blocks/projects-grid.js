d5a5db8b7f8127074ffc322551138ee6a62ec1fc
Oz Ramos <thepersonaldev@gmail.com>
2020-04-15 21:27:51 -0700

    #5 Displays project image in grid

--- assets/js/blocks/projects-grid.js
+++ assets/js/blocks/projects-grid.js
@@ -1,5 +1,7 @@
 import {registerBlockType} from '@wordpress/blocks'
 import {createElement} from '@wordpress/element'
+import {withSelect} from '@wordpress/data'
+import ServerSideRender from '@wordpress/server-side-render'
 
 const icon = createElement('svg', {viewBox: '0 0 512 512'},
 	createElement('path', {d: 'M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z'})
@@ -8,5 +10,9 @@ const icon = createElement('svg', {viewBox: '0 0 512 512'},
 registerBlockType('tpd/projects-grid', {
   title: 'Projects Grid',
   category: 'theme_blocks',
-  icon
+  icon,
+
+  edit: props => {
+    return <ServerSideRender block='tpd/projects-grid' attributes={props.attributes} />
+  }
 })
--- src/blocks/projects-grid.php
+++ src/blocks/projects-grid.php
@@ -11,6 +11,36 @@ add_action('init', function () {
   );
 
   register_block_type('tpd/projects-grid', [
-    'editor_script' => 'tpd-blocks-projects-grid'
+    'editor_script' => 'tpd-blocks-projects-grid',
+    'render_callback' => 'tpd_blocks_projects_grid_render'
   ]);
 });
+
+function tpd_blocks_projects_grid_render ($attributes, $content) {
+  $posts = get_posts([
+    'post_type' => 'tpd_project',
+    'posts_per_page' => 6
+  ]);
+
+  if (!count($posts)) {
+    return 'No Posts';
+  }
+  
+  global $post;
+  ob_start(); ?>
+    <div class="tpd-projects-grid row">
+      <?php foreach($posts as $post): setup_postdata($post); ?>
+        <div class="tpd-projects-item col-xs-12 col-lg-4">
+          <?php if (has_post_thumbnail($post->ID)): ?>
+            <div class="tpd-projects-item-cover">
+              <?php the_post_thumbnail() ?>
+            </div>
+          <?php endif ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  <?php
+  
+  wp_reset_postdata();
+  return ob_get_clean();
+}
