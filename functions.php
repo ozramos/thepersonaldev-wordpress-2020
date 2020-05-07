9256b3313eb80bfe18f70b94f4f5337aff01dcc9
Oz Ramos <thepersonaldev@gmail.com>
2020-05-07 15:51:11 -0700

    #22 Adds title tag

--- functions.php
+++ functions.php
@@ -46,6 +46,7 @@ add_action('init', function () {
  * Custom Logo
  */
 add_action('after_setup_theme', function () {
+  add_theme_support('title-tag');
   add_theme_support('custom-logo');
   add_theme_support('post-thumbnails', ['post', 'page', 'tpd_project']);
 });
